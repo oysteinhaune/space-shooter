0feea6aca0fb24ada202d4345dcdfd365b8a193d
Øystein <oysteinha@gmail.com>
2025-03-15 16:20:44 +0100

    Add engine pick to boss battle

--- scenes/boss_scene.gd
+++ scenes/boss_scene.gd
@@ -40,14 +40,30 @@ func _on_boss_1_character_area_entered(area):
 	
 	if boss_health > 0:
 		flash_and_stutter_boss()
-
-	if boss_health <= 0:
+	else:
+		var boss_position = $Boss1Character.global_position
+		$Boss1Character.queue_free()
+		spawn_boss_engine(boss_position)
 		show_victory_message()
+
+func _on_boss_engine_collision(body: Node) -> void:
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
 		
+func spawn_boss_engine(spawn_position):
+	await get_tree().create_timer(0.1).timeout
+	var boss_engine_scene = load("res://scenes/boss_engine.tscn")
+	var boss_engine = boss_engine_scene.instantiate()
+	boss_engine.global_position = spawn_position
+	get_parent().add_child(boss_engine)
+	
+	if boss_engine:
+		var area2d_node = boss_engine.get_node("Area2D")
+		if area2d_node and has_method("_on_boss_engine_collision"):
+			area2d_node.connect("body_entered", Callable(self, "_on_boss_engine_collision"))
 		
-func _input(event):
-	if event.is_action_pressed("reset") && boss_health == 0:
-		get_tree().change_scene_to_file("res://scenes/level.tscn")
+#func _input(event):
+	#if event.is_action_pressed("reset") && boss_health == 0:
+		#get_tree().change_scene_to_file("res://scenes/level.tscn")
 		
 func flash_player():
 	var player = $Player
@@ -79,7 +95,6 @@ func flash_and_stutter_boss():
 	boss_image.modulate = Color(1, 1, 1)
 
 func show_victory_message():
-	$Boss1Character.queue_free()
 	display_initial_message()
 
 func display_initial_message():
@@ -89,27 +104,26 @@ func display_initial_message():
 	label.set("theme/font_size", 60)
 	add_child(label)
 
-	await get_tree().create_timer(4).timeout
-
+	await get_tree().create_timer(3).timeout
 	label.queue_free()
 	display_victory_message()
 
 func display_victory_message():
 	var victory_label = Label.new()
-	victory_label.text = "With the Space Boss slain, the skies clear, and a sense of peace returns to the galaxy. \n\n" + "As the hero of this realm, your journey continues. \n" + "New adventures await, filled with uncharted worlds and legendary foes. \n" + "Gather your courage, for the stars are calling, and destiny awaits!"
-	victory_label.set_position(Vector2(400, 200))
-	victory_label.set("theme/font_size", 40)
+	victory_label.name = "VictoryLabel"  # Set name for easy removal
+	victory_label.text = "With the Space Boss slain, the skies clear, and a sense of peace returns to the galaxy.\n\n" + \
+		"As the hero of this realm, your journey continues.\n" + \
+		"New adventures await, filled with uncharted worlds and legendary foes.\n" + \
+		"Gather your courage, for the stars are calling, and destiny awaits!\n\n" + \
+		"You claim the fallen foe's engine, a relic of war.\n" + \
+		"With it, you shall navigate the asteroid field and journey to the lost world of Valtor-7...\n\n" + \
+		"Move over the engine to collect it and continue your journey."
+
+	victory_label.set_position(Vector2(250, 200))
+	victory_label.add_theme_color_override("font_color", Color(1, 1, 0))  # Yellow text
+
+	# Add to the scene
 	add_child(victory_label)
-
-	display_instruction_label()
-
-func display_instruction_label():
-	var instruction_label = Label.new()
-	instruction_label.text = "Press Space key to continue your journey."
-	instruction_label.set_position(Vector2(400, 390))
-	instruction_label.set("theme/font_size", 40)
-	add_child(instruction_label)
-
 
 func _on_music_finished():
 	$Music.play()
